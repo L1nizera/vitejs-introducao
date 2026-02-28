1895f5ef1ef125f3f2593df5197e72651093c617
Rhyan <l1nizerag0d@gmail.com>
2026-02-27 22:46:54 -0300

    27/02

---
+++ src/pages/sobre/index.jsx
@@ -0,0 +1,9 @@
+export default function Sobre () {
+    return (
+        <>
+        <h1>Sobre</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos voluptatibus maiores quisquam nobis quod suscipit ratione, maxime nam non earum officiis? Distinctio dolores odit nobis possimus facere! Delectus, expedita iste.</p>
+        </>
+        
+    )
+}
